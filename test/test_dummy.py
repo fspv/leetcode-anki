879f68a3d64dee9704fcd44f64c6dfd7b0d6dbb7
Pavel Safronov <pv.safronov@gmail.com>
2021-10-22 20:13:09 +0100

    Added QA workflows and made the code compliant

---
+++ test/test_dummy.py
@@ -0,0 +1,19 @@
+"""
+Just a placeholder
+"""
+
+
+class TestDummy:
+    """
+    Dummy test
+    """
+
+    @staticmethod
+    def test_do_nothing() -> None:
+        """Do nothing"""
+        assert True
+
+    @staticmethod
+    def test_do_nothing2() -> None:
+        """Do nothing"""
+        assert True
